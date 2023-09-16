bb971882875dce0214181a40b4032dffec20ef10
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-15 19:23:05 -0600

    Introducción a las promesas en JavaScript

--- Promesas.js
+++ Promesas.js
@@ -8,12 +8,27 @@ Promesas en JavaScript
 * entonces se manda a llamar el caso de REJECTED --> se procesa con la funcion catch(),
 ? Por lo tanto en una promesa tenemos DOS CAMINOS
 */
-'use strict';
+"use strict";
 
 // * Sintaxis basica para crear una promesa
 // Tenemos que pasarle dos parametros, estos tiene que ser funciones del tipo Callback
 // El primer parametro es una funcion Callback en caso de que haya sido resuelta correctamente la promesa then() RESOLVED
 // El segundo parametro es una funcion Callback en caso de que no hay sido resuelta correctamente REJECTED --> se procesa con la funcion catch().
 
-// ? Pero todo esto lo vamos a recibir como una funcion de tipo CALLBACK
-// ? Es por eso que estamos utilizando una funcion flecha a la cual le pasamos las dos funciones como parametros
+// * Pero todo esto lo vamos a recibir como una funcion de tipo CALLBACK
+// * Es por eso que estamos utilizando una funcion flecha a la cual le pasamos las dos funciones como parametros
+
+let miPromesa = new Promise((resolver, rechazar) => {
+  let expresion = true;
+
+  if (expresion) {
+    resolver("Resolvio correcto");
+  } else {
+    rechazar("Se produjo un error");
+  }
+});
+
+//Ejecutando la promesa con le metodo THEN y CATCH
+miPromesa
+  .then((valor) => console.log(valor))
+  .catch((valor) => console.log(valor));
