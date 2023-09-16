730bf4e58f820109a119a347beaa59083764719f
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-15 19:32:53 -0600

    Empleando promesa y funcion timeOut para procesos Asicronos

--- Funciones_setTimeout_y_Promesas.js
+++ Funciones_setTimeout_y_Promesas.js
@@ -1,2 +1,13 @@
 'use strict';
 
+
+let promesa = new Promise((resolver)=>{
+    console.log('Inicio promesa');
+    setTimeout(()=> resolver('saludos con promesa y timeout'),3000);
+    console.log('fin promesa');
+});
+
+promesa.then(
+    valor=> {console.log(valor)}
+);
+
